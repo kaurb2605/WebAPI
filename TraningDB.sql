0761d8ff0af9708b18f16fcd995267216c37a763
kaurb2605 <56531777+kaurb2605@users.noreply.github.com>
2019-10-16 14:58:08 +1100

    Add files via upload

---
+++ TraningDB.sql
@@ -0,0 +1,16 @@
+﻿USE [TraningDB]
+GO
+
+/****** Object: Table [dbo].[Training] Script Date: 10/16/2019 7:10:48 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Training] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (50) NOT NULL,
+    [StartDate] DATETIME      NOT NULL,
+    [EndDate]   DATETIME      NOT NULL
+);
